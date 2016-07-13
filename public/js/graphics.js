af17a1606c19f56464ea9fe8f291ef13ea474689
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-07-13 12:20:53 +0200

    Refactor key drawing methods

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -7,13 +7,13 @@ graphics.initializeGraphics = function initializeGraphics() {
 
 graphics.drawPiano = function drawPiano(x, y, width, height, keys) {
   graphics.drawRectangleBorder(x, y, width, height);
+  var whiteKeyWidth = Math.ceil(width / keys);
+  var delta = Math.ceil(whiteKeyWidth * (3 / 4));
   for (var i = 0; i < keys; i++) {
     //draw white keys
-    graphics.drawRectangleBorder(Math.ceil(x + i * Math.ceil(width / keys)), y, Math.ceil(width / keys), height);
+    graphics.drawWhiteKey(x, y, whiteKeyWidth, height, i);
     //draw black keys
-    if ((((i + 1) % 7) % 3) !== 0 || ((i + 1) % 7) == 6) { //<--- determines if we need a black key on current whitekeyposition
-      graphics.ctx.fillRect(Math.ceil(x + i * Math.ceil(width / keys) + Math.ceil(width / (keys * (4/3)))), y, Math.ceil(width / (keys * 2)), Math.ceil(height * 0.7));
-    }
+    graphics.drawBlackKey(x, y, whiteKeyWidth, delta, height, i);
   }
   
 
@@ -26,4 +26,14 @@ graphics.drawRectangleBorder = function drawRectangleBorder(x, y, width, height)
   graphics.ctx.lineTo(x, y + height);
   graphics.ctx.lineTo(x, y);
   graphics.ctx.stroke();
+}
+
+graphics.drawWhiteKey = function drawWhiteKey(x, y, width, height, pos) {
+  graphics.drawRectangleBorder(Math.ceil(x + pos * width), y, width, height);
+}
+
+graphics.drawBlackKey = function drawBlackKey(x, y, width, delta, height, pos) {
+   if ((((pos + 1) % 7) % 3) !== 0 || ((pos + 1) % 7) == 6) { //<--- determines if we need a black key on current whitekeyposition
+      graphics.ctx.fillRect(Math.ceil(x + pos * width + delta), y, width/2, Math.ceil(height * 0.7));
+    }
 }
